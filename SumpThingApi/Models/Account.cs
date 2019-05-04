af55377991e884d86c84d1386955c1f0a32533a7
Kalob Porter <kalob@kalob.net>
2019-05-04 09:12:00 -0400

    Prepare to add company model

--- SumpThingApi/Models/Account.cs
+++ SumpThingApi/Models/Account.cs
@@ -8,4 +8,11 @@ namespace SumpThingApi.Models {
     public List<UserAccount> UserAccounts { get; set; } = new List<UserAccount>();
     public List<Tank> Tanks { get; set; } = new List<Tank>();
   }
+
+  public class ResourceType {
+    private ResourceType(string value) { Value = value; }
+    public string Value { get; set; }
+    public static ResourceType User { get { return new ResourceType("user"); } }
+    public static ResourceType Company { get { return new ResourceType("company"); } }
+  }
 }
