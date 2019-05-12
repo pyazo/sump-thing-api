51a37b9767e20fa276ae28a4c452ad3f4bb1ca86
Kalob Porter <kalob@kalob.net>
2019-05-11 23:31:49 -0400

    Fix formatting

--- SumpThingApi/Maps/RegistrationMap.cs
+++ SumpThingApi/Maps/RegistrationMap.cs
@@ -6,16 +6,16 @@ namespace SumpThingApi.Maps
 {
   public class RegistrationMap
   {
-    public RegistrationMap(EntityTypeBuilder<Registration> e)
+    public RegistrationMap (EntityTypeBuilder<Registration> e)
     {
-      e.HasKey(x => x.Id);
-      e.ToTable("registrations");
+      e.HasKey (x => x.Id);
+      e.ToTable ("registrations");
 
-      e.Property(x => x.Id).HasColumnName("id");
-      e.Property(x => x.AccountId).HasColumnName("account_id");
-      e.Property(x => x.SumpThingBoxId).HasColumnName("sump_thing_box_id");
-      e.Property(x => x.CreatedAt).HasColumnName("created_at");
-      e.Property(x => x.UpdatedAt).HasColumnName("updated_at");
+      e.Property (x => x.Id).HasColumnName ("id");
+      e.Property (x => x.AccountId).HasColumnName ("account_id");
+      e.Property (x => x.SumpThingBoxId).HasColumnName ("sump_thing_box_id");
+      e.Property (x => x.CreatedAt).HasColumnName ("created_at");
+      e.Property (x => x.UpdatedAt).HasColumnName ("updated_at");
     }
   }
 }
